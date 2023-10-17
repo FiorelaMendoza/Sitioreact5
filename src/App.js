8f3fa25a69063aa4b9bf4a0c2c600a2fb968e7a6
FiorelaMendoza <fio.mendoza.capira@gmail.com>
2023-10-16 14:55:22 -1200

    modifique componentes

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import C01componente from './components/C01componente';
 import Variable from './components/C04variable';
-
+import MatrizOperaciones from './components/C07matrizOperaciones';
 
 function App() {
   return (
     <div>
      <C01componente/>
      <Variable xVariable = "Hola"/>
+     <MatrizOperaciones />
     </div>
   );
 }
